8aed890964555f77e3e18a5f3beb5672025974d9
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2022-06-25 00:59:30 +0300

    fix an error on user photo loading

--- src/components/UserItem/UserItem.jsx
+++ src/components/UserItem/UserItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cover from '../../assets/photo-cover.svg';
 
 const UserItem = ({ user }) => {
   const { photo, name, position, email, phone, registration_timestamp } = user;
@@ -6,7 +7,14 @@ const UserItem = ({ user }) => {
 
   return (
     <div>
-      <img src={photo} alt={name} />
+      <img
+        src={photo}
+        alt={name}
+        onError={({ currentTarget }) => {
+          currentTarget.onerror = null;
+          currentTarget.src = cover;
+        }}
+      />
       <p>{name}</p>
       <p>{position}</p>
       <address>
